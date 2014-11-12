75365754ff85f607fa2afedb13a03b09ca255c32
NikolaBozhkov <n.bozhkov.ads@gmail.com>
2014-11-12 21:49:46 +0200

    Windows Universal Controls homework

---
+++ WindowsUniversal/WindowsUniversalControls/ComboBoxTask/ComboBoxTask.Shared/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ComboBoxTask
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Uri uri = new Uri("http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg");
+            this.Image.Source = new BitmapImage(uri);
+            this.Image.MaxHeight = 100;
+            this.Image.MaxWidth = 200;
+        }
+    }
+}
